8de3acc7e75f7ed60e9a944cab6e3d46174f40f2
TOKO <tkakafid@gmail.com>
2018-03-27 14:01:46 +0200

    ajout de la syntaxe d'une page html

--- Acceuil.php
+++ Acceuil.php
@@ -1,4 +1,28 @@
 <?php 
-echo"ici c'est l'acceui" ;
+
+<!doctype html>
+
+<html>
+
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title>Introduction à JavaScript</title>
+
+</head>
+
+
+<body>
+
+    echo"ici c'est l'acceui" ;
 $var ="bonjour"; 
+
+    <script src="cours.js"></script>
+
+</body>
+
+
+</html>
 ?>
